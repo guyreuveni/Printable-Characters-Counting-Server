8eae343e0b4012d52a331fc9a5ac79c98e917d1e
Guy <guyreuveni98@gmail.com>
2023-06-30 18:33:08 +0300

    mid server

--- pcc_server.c
+++ pcc_server.c
@@ -9,7 +9,8 @@
 #include <errno.h>
 #include <arpa/inet.h>
 
-int is_proccesing_client = 0;
+ssize_t BUF_SIZE = 100000;
+int client_is_being_proccesed = 0;
 int its_time_to_terminate = 0;
 int printable_counter_is_initialized = 0;
 unsigned int printable_counter[127];
@@ -40,7 +41,7 @@ void terminate()
 
 void handle_sigint(int sig)
 {
-    if (is_proccesing_client == 0)
+    if (client_is_being_proccesed == 0)
     {
         terminate();
     }
@@ -50,9 +51,14 @@ void handle_sigint(int sig)
 int main(int argc, char const *argv[])
 {
     struct sigaction sa_sigint;
-    int listening_sock_fd;
-    struct sockaddr_in serv_addr;
+    int listening_sock_fd, con_fd, i;
+    struct sockaddr_in serv_addr, peer_addr;
     char *serv_port;
+    unsigned int pcc_counter, N, N_network, not_read, total_read;
+    unsigned int curr_client_printable_counter[127];
+    ssize_t now_read;
+    char buf[BUF_SIZE];
+
     socklen_t addrsize = sizeof(struct sockaddr_in);
 
     if (argc != 2)
@@ -79,7 +85,7 @@ int main(int argc, char const *argv[])
     listening_sock_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (listening_sock_fd < 0)
     {
-        perror("Failed to listen\n");
+        perror("Failed to create the listening socket\n");
         exit(1);
     }
 
@@ -97,9 +103,61 @@ int main(int argc, char const *argv[])
         exit(1);
     }
 
+    /*Creating the listening queue for the listening socket*/
+    if (listen(listening_sock_fd, 10) != 0)
+    {
+        perror("Failed to listen\n");
+        exit(1);
+    }
+
+    /*Handling clients*/
     while (its_time_to_terminate == 0)
     {
-        /* code */
+        /*Accepting a new client*/
+        con_fd = accept(listenfd, (struct sockaddr *)&peer_addr, &addrsize);
+        if (con_fd < 0)
+        {
+            perror("Accept failed\n");
+            exit(1);
+        }
+        client_is_being_proccesed = 1;
+        pcc_counter = 0;
+        for (i = 0; i <= 126; i++)
+        {
+            curr_client_printable_counter[i] = 0;
+        }
+
+        /*Reading N*/
+
+        not_read = sizeof(unsigned int);
+        total_read = 0;
+
+        while (not_read > 0)
+        {
+            now_read = read(con_fd, ((char *)(&N_network)) + total_read, not_read);
+            if (now_read < 0)
+            {
+                perror("Failed to read N\n");
+                exit(1);
+            }
+            total_read += now_read;
+            not_read -= now_read;
+        }
+
+        N = ntohl(N_network);
+
+        /*Reading data from client*/
+        total_read = 0;
+        
+        while (total_read < N){
+            now_read = read(con_fd, buf, BUF_SIZE);
+            if (now_read < 0)
+            {
+                perror("Failed to read N\n");
+                exit(1);
+            }
+            total_read += now_read;
+        }
     }
 
     terminate();
